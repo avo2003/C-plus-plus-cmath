bade90baf62cc3bcaea382d5cb9207872b5da1e1
avo2003 <avetis.f.y@tumo.org>
2019-06-26 22:50:55 +0400

    c++

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{ /*
+    cout << pow(2,5);
+    cout << sqrt(39);
+    cout << round(9.5);
+    cout << ceil(4.1);
+    cout << floor(4.8);
+    cout << fmax(3,10);
+    cout << fmin(3,10);*/
+
+  /*  int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout <<" you are " << age <<" years old";   */
+
+
+
+  /*   string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout <<" you r name is " << name ;   */
+
+
+
+     string name;
+    cout << "Enter your name: ";
+    getline(cin, name);
+    cout <<" hello " << name ;
+
+
+
+    return 0;
+}
